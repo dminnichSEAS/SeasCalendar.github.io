7375bb8104e08181493a2df200b4ba40e5c370fa
dminnichSEAS <148788360+dminnichSEAS@users.noreply.github.com>
2023-10-24 03:29:25 +0000

    test

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Check if booking.txt has been modified
-if git diff --quiet booking.txt; then
-  exit 0  # No changes, so exit without committing or pushing
+# Set your GitHub repository URL
+REPO_URL="https://github.com/dminnichSEAS/SeasCalendar.github.io.git"
+
+# Directory containing your Git repository
+REPO_DIR="/workspaces/SeasCalendar.github.io"
+
+# Check if bookings.txt is modified
+if git -C "$REPO_DIR" status --porcelain | grep -q "bookings.txt"; then
+    # Commit the changes
+    git -C "$REPO_DIR" add bookings.txt
+    git -C "$REPO_DIR" commit -m "Automatic commit for modified bookings.txt"
+
+    # Push the changes to the remote repository
+    git -C "$REPO_DIR" push origin master  # Change 'master' to your branch name if needed
+    echo "Changes committed and pushed to the repository."
+else
+    echo "No modifications in bookings.txt."
 fi
-
-# Commit the changes with the message "auto"
-git add bookings.txt
-git commit -m "auto"
-
-# Push the changes
-git push
