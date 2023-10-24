c65a34ec59318bd655002fc0352761e74398e1e2
dminnichSeas <dminnich@seascsfw.org>
2023-10-24 09:24:37 -0400

    auto

--- auto_commit_distant.py
+++ auto_commit_distant.py
@@ -1,15 +1,14 @@
-# run with --> python auto_commit_distant.py
 import os
 import subprocess
 import time
+import shutil
 
 # Define the repository path
-repo_path = 'C:/Users/seas/Desktop/TSv3-main/bookings.txt'
-
+repo_path = 'C:/Users/seas/Desktop/TSv3-main'
 
 # Define the file to monitor for changes
 file_to_monitor = 'bookings.txt'
-
+script_file_path = os.path.abspath(__file__)  # Get the absolute path of this script
 
 def is_file_modified(file_path):
     # Check if the file has been modified
@@ -37,6 +36,14 @@ def main():
             print(f"Changes in {file_to_monitor} have been committed and pushed.")
         else:
             print(f"No changes in {file_to_monitor}.")
+
+        # Check if this script has been modified
+        if is_file_modified(script_file_path):
+            print("Script has been modified. Copying it to the repository and committing...")
+            script_file_destination = os.path.join(repo_path, os.path.basename(script_file_path))
+            shutil.copyfile(script_file_path, script_file_destination)
+            git_commit_and_push(script_file_destination)
+            print("Script has been copied and committed.")
 
         # Wait for a certain interval before checking again (e.g., every 60 seconds)
         time.sleep(10)
