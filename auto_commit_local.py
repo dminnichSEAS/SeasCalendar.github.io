58420e5f5f0da9a3a144dc4466ad0a04a5ea67b3
dminnichSeas <dminnich@seascsfw.org>
2023-12-18 11:04:36 -0500

    uh

--- auto_commit_local.py
+++ auto_commit_local.py
@@ -6,10 +6,8 @@ import time
 # Define the repository path
 repo_path = 'C:/Users/seas/Desktop/CalendarRepoClone/SeasCalendar.github.io'
 
-
 # Define the file to monitor for changes
 file_to_monitor = 'bookings.txt'
-
 
 def is_file_modified(file_path):
     # Check if the file has been modified
@@ -18,7 +16,7 @@ def is_file_modified(file_path):
         return not output.startswith("??")  # Check if it's not an untracked file
     except subprocess.CalledProcessError:
         return False
-
+  
 def git_commit_and_push(file_path):
     # Commit and push changes in the specified file
     subprocess.call(['git', 'add', file_path], cwd=repo_path)
@@ -29,7 +27,17 @@ def main():
     # Set the working directory
     os.chdir(repo_path)
 
-    while True:
+    while True: 
+        # Open the source file in read mode
+        with open('C:/Users/seas\Desktop/TSv3-main/bookings.txt', 'r') as source_file:
+    # Read the content
+            content = source_file.read()
+
+# Open the destination file in append mode
+            with open('bookings.txt', 'a') as destination_file:
+    # Write the content
+                destination_file.write(content)
+
         # Check if the specified file has been modified
         if is_file_modified(file_to_monitor):
             print(f"Changes detected in {file_to_monitor}. Committing and pushing...")
@@ -39,7 +47,8 @@ def main():
             print(f"No changes in {file_to_monitor}.")
 
         # Wait for a certain interval before checking again (e.g., every 60 seconds)
-        time.sleep(10)
+        time.sleep(100)
+
 
 if __name__ == '__main__':
     main()
